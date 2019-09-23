0dfc2d24c70b19c13127f09987d59e94d10e107b
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-09-23 17:23:38 +0100

    Advanced TypeScript

--- mod-3-advanced/src/demo.js
+++ mod-3-advanced/src/demo.js
@@ -1,18 +1,37 @@
-var Operation;
-(function (Operation) {
-    Operation[Operation["Add"] = 0] = "Add";
-    Operation[Operation["Subtract"] = 1] = "Subtract";
-})(Operation || (Operation = {}));
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var Operation2;
+(function (Operation2) {
+    Operation2["Add"] = "Add";
+    Operation2["Subtract"] = "Subtract";
+    // Multiply
+})(Operation2 || (Operation2 = {}));
+// enum Operation {
+//   Add=1,
+//   Subtract=2
+//   // Multiply
+// }
 class Calculator {
     calculate(operation, x, y) {
         let result = Number.NaN;
         switch (operation) {
-            case Operation.Add:
+            case "Add":
                 result = this.add(x, y);
                 break;
-            case Operation.Subtract:
+            case "Subtract":
                 result = this.subtract(x, y);
                 break;
+            default:
+                const n = operation;
+                throw Error("Unexpected value " + n);
         }
         return result;
     }
@@ -29,30 +48,83 @@ const add = document.getElementById("add");
 const subtract = document.getElementById("subtract");
 const result = document.getElementById("result");
 const calculator = new Calculator();
-add.addEventListener("click", () => {
-    result.textContent = calculator
-        .calculate(Operation.Add, +x.value, +y.value)
-        .toString();
-});
-subtract.addEventListener("click", () => {
-    result.textContent = calculator
-        .calculate(Operation.Subtract, +x.value, +y.value)
-        .toString();
-});
-function fetchJokes() {
-    fetch("http://api.icndb.com/jokes/random/10/?limitTo=[nerdy]&escape=javascript");
-    return {
-        value: [
-            {
-                id: 510,
-                joke: "Chuck Norris can compile syntax errors.",
-                categories: ["nerdy"]
-            }
-        ]
-    };
+if (result && x) {
+    if (add) {
+        add.addEventListener("click", () => {
+            result.textContent = calculator
+                .calculate("Add", +x.value, +y.value)
+                .toString();
+        });
+    }
+    subtract &&
+        subtract.addEventListener("click", () => {
+            result.textContent = calculator
+                .calculate("Subtract", +x.value, +y.value)
+                .toString();
+        });
+}
+function fetchThings(url) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const rsp = yield fetch(url);
+        const json = yield rsp.json();
+        return json;
+        // return {
+        //   value: [
+        //     {
+        //       id: 510,
+        //       joke: "Chuck Norris can compile syntax errors.",
+        //       categories: ["nerdy"]
+        //     }
+        //   ]
+        // };
+    });
 }
 function printJokes() {
-    const jokes = fetchJokes();
-    console.table(jokes.value);
+    return __awaiter(this, void 0, void 0, function* () {
+        const url = "http://api.icndb.com/jokes/random/10/?limitTo=[nerdy]&escape=javascript";
+        const jokes = (yield fetchThings(url));
+        console.table(jokes.value);
+    });
 }
 printJokes();
+let c = {
+    type: "cat",
+    name: "",
+    sleep() { }
+};
+// let d: CatAndDog = {
+// type: 'dog',
+//   name: "",
+//   sleep() {},
+//   eat() {}
+// };
+// if (d.type === 'cat'&'dog'){
+// }
+function sellPet(pet) {
+    console.log("Selling", pet.name);
+}
+function isCat(pet) {
+    if (typeof pet.sleep === "function") {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+class Snake {
+}
+function feedPet(pet) {
+    // if (pet && typeof pet.eat === "function") {
+    //   pet.eat();
+    // }
+    if (pet.type === "dog") {
+        pet.eat();
+        // pet.name = "ssss";
+    }
+    if (isCat(pet)) {
+        pet.sleep();
+    }
+    if (pet instanceof Snake) {
+    }
+    console.log("Feedign", pet.name);
+}
--- mod-3-advanced/src/demo.ts
+++ mod-3-advanced/src/demo.ts
@@ -1,34 +1,48 @@
-enum Operation {
-  Add,
-  Subtract
+enum Operation2 {
+  Add = "Add",
+  Subtract = "Subtract"
+  // Multiply
 }
 
+// console.log(Object.keys(Operation2));
+
+type Operation = "Add" | "Subtract";
+
+// enum Operation {
+//   Add=1,
+//   Subtract=2
+//   // Multiply
+// }
+
 class Calculator {
-  calculate(operation: Operation, x: number, y: number): number {
+  calculate(operation: Operation, x: number, y: number) {
     let result = Number.NaN;
 
     switch (operation) {
-      case Operation.Add:
+      case "Add":
         result = this.add(x, y);
         break;
-      case Operation.Subtract:
+      case "Subtract":
         result = this.subtract(x, y);
         break;
+      default:
+        const n: never = operation;
+        throw Error("Unexpected value " + n);
     }
 
     return result;
   }
 
-  private add(x: number, y: number): number {
+  private add(x: number, y: number) {
     return x + y;
   }
 
-  private subtract(x: number, y: number): number {
+  private subtract(x: number, y: number) {
     return x - y;
   }
 }
 
-const x = document.getElementById("x") as HTMLInputElement;
+const x = document.getElementById("x") as HTMLInputElement | null;
 const y = <HTMLInputElement>document.getElementById("y");
 const add = document.getElementById("add");
 const subtract = document.getElementById("subtract");
@@ -36,37 +50,122 @@ const result = document.getElementById("result");
 
 const calculator = new Calculator();
 
-add.addEventListener("click", () => {
-  result.textContent = calculator
-    .calculate(Operation.Add, +x.value, +y.value)
-    .toString();
-});
+if (result && x) {
+  if (add) {
+    add.addEventListener("click", () => {
+      result.textContent = calculator
+        .calculate("Add", +x.value, +y.value)
+        .toString();
+    });
+  }
 
-subtract.addEventListener("click", () => {
-  result.textContent = calculator
-    .calculate(Operation.Subtract, +x.value, +y.value)
-    .toString();
-});
-
-function fetchJokes() {
-  fetch(
-    "http://api.icndb.com/jokes/random/10/?limitTo=[nerdy]&escape=javascript"
-  );
-
-  return {
-    value: [
-      {
-        id: 510,
-        joke: "Chuck Norris can compile syntax errors.",
-        categories: ["nerdy"]
-      }
-    ]
-  };
+  subtract &&
+    subtract.addEventListener("click", () => {
+      result.textContent = calculator
+        .calculate("Subtract", +x.value, +y.value)
+        .toString();
+    });
 }
 
-function printJokes() {
-  const jokes = fetchJokes();
+async function fetchThings(url: string) {
+  const rsp = await fetch(url);
+  const json: unknown = await rsp.json();
+
+  return json;
+  // return {
+  //   value: [
+  //     {
+  //       id: 510,
+  //       joke: "Chuck Norris can compile syntax errors.",
+  //       categories: ["nerdy"]
+  //     }
+  //   ]
+  // };
+}
+
+type JokesResponse = {
+  value: {
+    joke: string;
+  }[];
+};
+async function printJokes() {
+  const url =
+    "http://api.icndb.com/jokes/random/10/?limitTo=[nerdy]&escape=javascript";
+
+  const jokes = (await fetchThings(url)) as JokesResponse;
   console.table(jokes.value);
 }
 
 printJokes();
+
+type Cat = {
+  type: "cat";
+  name: string;
+  sleep(): void;
+};
+
+type Dog = {
+  type: "dog";
+  name: string;
+  eat(): void;
+};
+
+type CatOrDog = Cat | Dog;
+type CatAndDog = Cat & Dog;
+
+let c: CatOrDog = {
+  type: "cat",
+  name: "",
+  sleep() {}
+};
+
+// let d: CatAndDog = {
+// type: 'dog',
+//   name: "",
+//   sleep() {},
+//   eat() {}
+// };
+
+// if (d.type === 'cat'&'dog'){
+
+// }
+
+function sellPet(pet: CatOrDog) {
+  console.log("Selling", pet.name);
+}
+
+function isCat(pet: any): pet is Cat {
+  if (typeof pet.sleep === "function") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+type IsCatResult = ReturnType<typeof isCat>;
+
+class Snake {}
+
+function feedPet(pet: Readonly<CatOrDog>) {
+  // if (pet && typeof pet.eat === "function") {
+  //   pet.eat();
+  // }
+
+  if (pet.type === "dog") {
+    pet.eat();
+
+    // pet.name = "ssss";
+  }
+
+  if (isCat(pet)) {
+    pet.sleep();
+  }
+
+  if (pet instanceof Snake) {
+  }
+
+  console.log("Feedign", pet.name);
+}
+
+type K = Pick<Cat, "name" | "sleep">;
+type K2 = Omit<Cat, "name" | "sleep">;
